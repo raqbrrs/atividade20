595a56a01fc48d38ea86d06dc4b7231a174bc49b
raqbrrs <raquelbrrs07@gmail.com>
2024-09-18 15:52:07 -0300

    soma dos num pares

---
+++ atividade20.py
@@ -0,0 +1,9 @@
+# Crie um programa que utilize uma estrutura de repetição para somar todos os números pares de 1 a 100 e exiba o resultado.
+
+soma = 0
+for n in range(1, 101):
+    if n %2 == 0:
+        soma += n
+
+print (soma)
+        
